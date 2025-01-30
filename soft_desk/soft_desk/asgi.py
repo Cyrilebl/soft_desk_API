7482f3cd0390b81f20a9f2cfb040e346fd5119cf
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-01-30 15:56:30 +0100

    delete default comments

--- soft_desk/soft_desk/asgi.py
+++ soft_desk/soft_desk/asgi.py
@@ -1,16 +1,7 @@
-"""
-ASGI config for soft_desk project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soft_desk.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soft_desk.settings")
 
 application = get_asgi_application()
