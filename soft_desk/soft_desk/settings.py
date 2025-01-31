05a36161e9738b2a78c14829c70edc6b7d53ef61
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-01-31 23:57:26 +0100

    setup simplejwt

--- soft_desk/soft_desk/settings.py
+++ soft_desk/soft_desk/settings.py
@@ -27,13 +27,12 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "api",
     "rest_framework",
+    "rest_framework_simplejwt",
 ]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
-        "rest_framework.authentication.BasicAuthentication",
-        "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
--- soft_desk/soft_desk/urls.py
+++ soft_desk/soft_desk/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from api.views import (
     UserViewSet,
@@ -25,5 +26,7 @@ api_patterns = (router.urls, app_name)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/", include(api_patterns, namespace=app_name)),
 ]
