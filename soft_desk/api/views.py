018fbba3dc988adcb96c2ca8307e302f250dc9e6
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-01-31 01:15:43 +0100

    improve contributor view

--- soft_desk/api/views.py
+++ soft_desk/api/views.py
@@ -1,4 +1,8 @@
-from rest_framework.exceptions import ValidationError, PermissionDenied
+from rest_framework.exceptions import (
+    ValidationError,
+    PermissionDenied,
+    MethodNotAllowed,
+)
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
@@ -16,6 +20,9 @@ class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
 
     def get_permissions(self):
+        """
+        Any user can create an account
+        """
         if self.action == "create":
             return [AllowAny()]
         return [IsAuthenticated()]
@@ -39,18 +46,30 @@ class ContributorViewSet(ModelViewSet):
     serializer_class = ContributorSerializer
     queryset = Contributor.objects.all()
 
+    def get_permissions(self):
+        """
+        Disable PUT / PATCH.
+        """
+        if self.action in ["update", "partial_update"]:
+            raise MethodNotAllowed(
+                "PUT and PATCH methods are not allowed for contributors."
+            )
+        return super().get_permissions()
+
     def perform_create(self, serializer):
-        user = self.request.user
+        user_id = self.request.data.get("user")
         project_id = self.request.data.get("project")
 
-        try:
-            project = Project.objects.get(id=project_id)
-        except Project.DoesNotExist:
-            raise ValidationError({"project": "Invalid project ID."})
+        user = User.objects.get(id=user_id)
+        project = Project.objects.get(id=project_id)
 
-        if project.author != user:
+        if project.author != self.request.user:
             raise PermissionDenied(
                 "You are not allowed to add contributors to this project."
+            )
+        if user == project.author:
+            raise ValidationError(
+                ({"user": "The project author cannot add themselves as a contributor."})
             )
 
         serializer.save(user=user, project=project)
